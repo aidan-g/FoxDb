18d391d9fd3fa5b92ecf979e7051eae3d68657a8
aidan <aidan.gilbert@live.co.uk>
2018-01-14 01:13:34 +0000

    Using better (more exact) assertion of test data.
     Turns out some columns were not included in assertions.
     New strategy doesn't require maintenance.

--- FoxDb.Tests/BehaviourTests.cs
+++ FoxDb.Tests/BehaviourTests.cs
@@ -35,12 +35,45 @@ namespace FoxDb
             }
         }
 
+        [Test]
+        public void Int32Columns()
+        {
+            var provider = new SQLiteProvider(Path.Combine(CurrentDirectory, "test.db"));
+            var database = new Database(provider);
+            using (var transaction = database.Connection.BeginTransaction())
+            {
+                database.Execute(database.QueryFactory.Create(CreateSchema), transaction: transaction);
+                var set = database.Set<Pear>(transaction);
+                var data = new List<Pear>();
+                set.Clear();
+                this.AssertSequence(data, set);
+                data.AddRange(new[]
+                {
+                    new Pear() { Field1 = "1_1", Field2 = "1_2", Field3 = "1_3", Field4 = 1 },
+                    new Pear() { Field1 = "2_1", Field2 = "2_2", Field3 = "2_3", Field4 = 2 },
+                    new Pear() { Field1 = "3_1", Field2 = "3_2", Field3 = "3_3", Field4 = 3 }
+                });
+                set.AddOrUpdate(data);
+                this.AssertSequence(data, set);
+                data[1].Field4 = 4;
+                set.AddOrUpdate(data);
+                this.AssertSequence(data, set);
+                transaction.Rollback();
+            }
+        }
+
         [Table(Name = "Test001")]
         public class Orange : Test001
         {
             public virtual int? Field4 { get; set; }
 
             public virtual double? Field5 { get; set; }
+        }
+
+        [Table(Name = "Test001")]
+        public class Pear : Test001
+        {
+            public virtual int Field4 { get; set; }
         }
     }
 }
