91976cf23dd99f73003f15250e31394e01f32b0d
aidan <aidan.gilbert@live.co.uk>
2018-01-14 01:34:48 +0000

    Enable mapping from long (INTEGER) to int.

---
+++ FoxDb.Core/Interfaces/Utility/IPropertyAccessorFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace FoxDb.Interfaces
+{
+    public interface IPropertyAccessorFactory
+    {
+        Expression Create(PropertyInfo property);
+
+        IPropertyAccessor<T, TValue> Create<T, TValue>(Expression expression);
+
+        IPropertyAccessor<T, TValue> Create<T, TValue>(PropertyInfo property);
+    }
+}
--- FoxDb.Core/Utility/PropertyAccessorFactory.cs
+++ FoxDb.Core/Utility/PropertyAccessorFactory.cs
@@ -5,55 +5,36 @@ using System.Reflection;
 
 namespace FoxDb
 {
-    public static class PropertyAccessorFactory
+    public class PropertyAccessorFactory : IPropertyAccessorFactory
     {
-        public static readonly IPropertyAccessorStrategy Strategy = new LambdaPropertyAccessorStrategy();
+        public PropertyAccessorFactory(bool conversionEnabled)
+        {
+            this.Strategy = new LambdaPropertyAccessorStrategy(conversionEnabled);
+        }
 
-        public static Expression Create(PropertyInfo property)
+        public IPropertyAccessorStrategy Strategy { get; private set; }
+
+        public Expression Create(PropertyInfo property)
         {
             var parameter = Expression.Parameter(property.DeclaringType);
             return Expression.Lambda(Expression.Property(parameter, property), parameter);
         }
 
-        public static IPropertyAccessor<T, TValue> Create<T, TValue>(Expression expression)
+        public IPropertyAccessor<T, TValue> Create<T, TValue>(Expression expression)
         {
-            var property = GetLambdaProperty<T>(expression);
-            return Create<T, TValue>(property);
+            var property = expression.GetLambdaProperty<T>();
+            return this.Create<T, TValue>(property);
         }
 
-        public static IPropertyAccessor<T, TValue> Create<T, TValue>(PropertyInfo property)
+        public IPropertyAccessor<T, TValue> Create<T, TValue>(PropertyInfo property)
         {
             if (property.GetGetMethod() == null || property.GetSetMethod() == null)
             {
                 throw new NotImplementedException();
             }
-            var get = Strategy.CreateGetter<T, TValue>(property);
-            var set = Strategy.CreateSetter<T, TValue>(property);
+            var get = this.Strategy.CreateGetter<T, TValue>(property);
+            var set = this.Strategy.CreateSetter<T, TValue>(property);
             return new PropertyAccessor<T, TValue>(property, get, set);
-        }
-
-        public static PropertyInfo GetLambdaProperty<T>(Expression expression)
-        {
-            if (expression.NodeType != ExpressionType.Lambda)
-            {
-                throw new NotImplementedException();
-            }
-            var lambda = expression as LambdaExpression;
-            if (lambda.Body.NodeType != ExpressionType.MemberAccess)
-            {
-                throw new NotImplementedException();
-            }
-            var member = lambda.Body as MemberExpression;
-            if (!(member.Member is PropertyInfo))
-            {
-                throw new NotImplementedException();
-            }
-            var property = member.Member as PropertyInfo;
-            if (property.DeclaringType == typeof(T))
-            {
-                return property;
-            }
-            return typeof(T).GetProperty(property.Name, BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly) ?? typeof(T).GetProperty(property.Name);
         }
 
         private class PropertyAccessor<T, TValue> : IPropertyAccessor<T, TValue>
